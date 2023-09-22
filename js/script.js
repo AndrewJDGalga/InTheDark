c3d4274d82f580c8b48b47607f2a3f5dd65a1695
Andrew DGalga <ex.spake@gmail.com>
2023-09-22 14:42:29 -0500

    switched visual timer to id, realized hidden was set as id (something is wrong with me), added invisible class so restart doesn't pop scene when added. Gonna try that out.

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const play = document.getElementById('itd-play');
+const pause = document.getElementById('itd-pause');
+const restart = document.getElementById('itd-restart');
+//const visualTimer = document.getElementsByClassName()
