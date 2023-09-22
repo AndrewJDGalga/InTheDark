c0c9fce162e4108a21b2e0e13623c8c6906ffb87
Andrew DGalga <ex.spake@gmail.com>
2023-09-22 14:54:17 -0500

    JS flip between play and pause buttons.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 const play = document.getElementById('itd-play');
 const pause = document.getElementById('itd-pause');
 const restart = document.getElementById('itd-restart');
-//const visualTimer = document.getElementsByClassName()
+const visualTimer = document.getElementById('itd-visual_timer');
+const timoutNotice = document.getElementsByClassName('itd-timeout')[0];
+
+const flipButtonClass = (button1, button2, className) => {
+    
+};
+
+play.addEventListener('click', ()=>{
+    pause.classList.remove('hidden');
+    play.classList.add('hidden');
+});
+
+pause.addEventListener('click', ()=>{
+    pause.classList.add('hidden');
+    play.classList.remove('hidden');
+});
