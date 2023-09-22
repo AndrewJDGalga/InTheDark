0188e5fa2fb3b81021f275573875ef571c0ebed0
Andrew DGalga <ex.spake@gmail.com>
2023-09-22 15:10:30 -0500

    reveal counter visual on play start, added timer starting reference in seconds, added current time tracking variable.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const pause = document.getElementById('itd-pause');
 const restart = document.getElementById('itd-restart');
 const visualTimer = document.getElementById('itd-visual_timer');
 const timoutNotice = document.getElementsByClassName('itd-timeout')[0];
+const timerStartSeconds = 240;
+let timerCurrent = timerStartSeconds;
 
 const flipButtonClass = (button1, button2, className) => {
     button1.classList.remove(className);
@@ -11,8 +13,10 @@ const flipButtonClass = (button1, button2, className) => {
 
 play.addEventListener('click', ()=>{
     flipButtonClass(pause, play, 'hidden');
+    visualTimer.classList.remove('invisible');
 });
 
 pause.addEventListener('click', ()=>{
     flipButtonClass(play, pause, 'hidden');
 });
+
