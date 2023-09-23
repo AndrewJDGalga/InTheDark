abb361097a90cbc36fb242303d88e2e362b5150d
Andrew DGalga <ex.spake@gmail.com>
2023-09-23 09:42:15 -0500

    timer experimentation. Thought making a timer in JS would be a simple operation, but there's enough varities in solutions and accuracy that I lost track of which approach I should even take. This is supposed to be a simple timer with a low accuracy requirement. Going to set aside the state machine I was creating and try to make something simple and straight forward with space for the required breakpoints. I'm opting for 1 interval & 1 timeout instead of 4 timeouts because: 1. I need at least 1 timeout to automatically stop when I also have an infinite counter, as far as I'm aware. 2. I'd rather deal with 1 interval checking proximity to the 3 remaining breakpoints every second than needing to work backwards from start time to save the current time on pause for a timeout.

--- js/basicTimer.js
+++ js/basicTimer.js
@@ -5,13 +5,55 @@
 
 class BasicTimer {
     constructor(startingTime){
+        this.intervalID = null;
         this.currentTime = 0;
         this.startingTime = startingTime;
         this.states = {
             STOPPED : Symbol("stopped"),
             RUNNING : Symbol("running"),
             PAUSED : Symbol("paused"),
+            RESTART : Symbol("restart"),
         };
         this.state = this.states.STOPPED;
+    }
+    process() {
+        switch(this.state) {
+            /*
+            case RESTART:
+                if(this.currentTime < this.startingTime) {
+                    this.currentTime = this.startingTime;
+                }*/
+            case RUNNING:
+                this.intervalID = setInterval(()=>{
+
+                });
+                break;
+            case PAUSED:
+                if(this.intervalID) {
+                    clearInterval(this.intervalID);
+                    this.intervalID = null;
+                }
+                break;
+            case STOPPED:
+                if(this.currentTime < this.startingTime) {
+                    this.currentTime = this.startingTime;
+                }
+        }
+    }
+    restart() {
+        this.state = this.states.RESTART;
+        process();
+    }
+    start() {
+        this.state = this.states.RUNNING;
+        process();
+    }
+    stop() {
+        this.state = this.states.STOPPED;
+        process();
+    }
+    pause() {
+        this.state = this.states.PAUSED;
+        process();
     }
 }
--- js/script.js
+++ js/script.js
@@ -3,8 +3,37 @@ const pause = document.getElementById('itd-pause');
 const restart = document.getElementById('itd-restart');
 const visualTimer = document.getElementById('itd-visual_timer');
 const timoutNotice = document.getElementsByClassName('itd-timeout')[0];
-const timerStartSeconds = 240;
+const timerStartSeconds = 10;
 let timerCurrent = timerStartSeconds;
+
+const seconds = 1000;
+let break1 = 2;
+let break2 = 5;
+let break3 = 7;
+let intervalId = null;
+let timeoutId = null
+
+const timer = () => {
+    if(intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+        clearTimeout(timeoutId);
+        timeoutId = null;
+        timerCurrent = timerStartSeconds;
+    } else {
+        intervalId = setInterval(()=>{
+            timerCurrent--;
+            console.log(timerCurrent);
+            if(timerCurrent === break1) console.log('break1');
+            if(timerCurrent === break2) console.log('break2');
+            if(timerCurrent === break3) console.log('break3');
+        }, seconds);
+        timeoutId = setTimeout(()=>{
+            clearInterval(intervalId);
+            console.log('stopped, timeleft: ' + timerCurrent);
+        }, timerStartSeconds * seconds);
+    }
+};
 
 const flipButtonClass = (button1, button2, className) => {
     button1.classList.remove(className);
@@ -14,9 +43,10 @@ const flipButtonClass = (button1, button2, className) => {
 play.addEventListener('click', ()=>{
     flipButtonClass(pause, play, 'hidden');
     visualTimer.classList.remove('invisible');
+    timer();
 });
 
 pause.addEventListener('click', ()=>{
     flipButtonClass(play, pause, 'hidden');
+    timer();
 });
-
