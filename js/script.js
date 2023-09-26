1faadbaf4cee866e95d0837ab4755f6a83eeaace
Andrew DGalga <ex.spake@gmail.com>
2023-09-26 14:15:43 -0500

    basic audio test 2.

--- js/script.js
+++ js/script.js
@@ -20,8 +20,8 @@ let fresh = true;
 const audioTrack = new Audio('./assets/audio/jumpscare_test.wav');
 const audioSting = new Audio('./assets/audio/Jumpscare Sound Effect.mp3');
 
-audioTrack.addEventListener('canplaythrough', () =>{
-    audioTrack.play();
+audioSting.addEventListener('canplaythrough', () =>{
+    audioSting.play();
 });
 
 const getRanTimePoint = (originalTime, min=0) =>{
