63863bef58f694ce12cb245093299da98471c88a
Andrew DGalga <ex.spake@gmail.com>
2023-10-02 14:41:49 -0500

    settings HTML boilerplate and settingsStyle.

--- php/appSettings.php
+++ php/appSettings.php
@@ -1,2 +1,11 @@
-<?php
-    echo "welcome";
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>In The Dark Timer Settings</title>
+</head>
+<body>
+    
+</body>
+</html>  
