e0a5bd3fcfe797f62d059f476931c5cb2d36f3a7
Andrew DGalga <ex.spake@gmail.com>
2023-10-03 11:59:22 -0500

    cookiecheck app settings. Not propogating to server as performance untrusted.

--- php/appSettings.php
+++ php/appSettings.php
@@ -1,6 +1,19 @@
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
+    <?php 
+        $env = file_get_contents('.env');
+        $sploded_env = explode("\n", $env);
+        $cookienameRaw = explode("=", $sploded_env[2]);
+        $cookievalRaw = explode("=", $sploded_env[3]);
+        $cookiename = trim($cookienameRaw[1]);
+        $cookieval = trim($cookievalRaw[1]);
+
+        if(!isset($_COOKIE[$cookiename]) || !$_COOKIE[$cookiename] === $cookieval){
+            header("Location: ../index.html");
+            die();
+        }
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>In The Dark Timer Settings</title>
