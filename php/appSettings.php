51f86d2f982e2ea1d4cd02f84bd600e50b01f0f2
Andrew DGalga <ex.spake@gmail.com>
2023-10-03 14:27:53 -0500

    appSettings form and back button.

--- php/appSettings.php
+++ php/appSettings.php
@@ -23,9 +23,26 @@
 <body>
     <main>
         <form method="POST" action="updateData.php" enctype="multipart/form-data">
-            
+        <label for="timerLength">Timer length in seconds: </label>
+            <input type="number" name="timerLength" step="0.01" value="<?php echo $readData->timerEndSeconds ?>">
+            <label for="animOcillationMin">Animation ocillation minimum in minutes: </label>
+            <input type="number" name="animOcillationMin" step="0.01" value="<?php echo $readData->oscAnimMin ?>">
+            <label for="animOcillationMax">Animation ocillation maximum in minutes: </label>
+            <input type="number" name="animOcillationMax" step="0.01" value="<?php echo $readData->oscAnimMax ?>">
+            <label for="animCheckTimeframe">Animation recheck timeframe in seconds: </label>
+            <input type="number" name="animCheckTimeframe" step="0.01" value="<?php echo $readData->oscillatFreq ?>">
+            <label for="musicFile">Main audio: </label>
+            <input type="file" name="musicFile">
+            <label for="stingFile">Audio sting: </label>
+            <input type="file" name="stingFile">
+            <input type="submit" value="Submit">
         </form>
-        <button id="back">Back</button>
+        <button id="itd-back">Back</button>
     </main>
+    <script>
+        document.getElementById('itd-back').addEventListener('click', ()=>{
+            window.location.replace('../index.html');
+        });
+    </script>
 </body>
 </html>  
