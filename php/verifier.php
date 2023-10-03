9c9562489b9c550e849c45245aa3547eb2d93630
Andrew DGalga <ex.spake@gmail.com>
2023-10-03 12:19:01 -0500

    verifier initial variables.

--- php/verifier.php
+++ php/verifier.php
@@ -1,2 +1,12 @@
 <?php
-    
+    if(isset($_POST["itd-uname"])) {
+        $env = file_get_contents('.env');
+        $sploded_env = explode("\n", $env);
+        $unameRaw = explode("=", $sploded_env[0]);
+        $upassRaw = explode("=", $sploded_env[1]);
+        $uname = trim($unameRaw[1]);
+        $upass = trim($upassRaw[1]);
+
+        $enteredName = htmlspecialchars($_POST["itd-uname"]);
+        $enteredPass = htmlspecialchars($_POST["itd-upass"]);
+    }
